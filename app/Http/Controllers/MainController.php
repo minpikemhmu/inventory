df718c7ed831d1b9c681117d7eb84bc709f43c92
minpikehmu <minpikehmu10@gmail.com>
2020-12-04 14:32:27 +0630

    error remark2

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -661,8 +661,7 @@ public function profit(Request $request){
    $end_date=$request->end_date;
    //dd($start_date);
   $ways=DeliveryMan::with('ways')->whereHas('ways',function($query) use($start_date,$end_date){
-    $query->whereBetween('created_at', [$start_date.' 00:00:00',$end_date.' 23:59:59']);
-    $query->orWhere('status_code','001');
+    $query->whereBetween('created_at', [$start_date.' 00:00:00',$end_date.' 23:59:59'])->where('status_code','001');
   })
   //->orWhereDoesntHave('ways')
   ->with('pickups')->orwhereHas('pickups',function($query) use($start_date,$end_date){
--- database/seeds/ExpensetypesTableSeeder.php
+++ database/seeds/ExpensetypesTableSeeder.php
@@ -15,5 +15,6 @@ class ExpensetypesTableSeeder extends Seeder
          ExpenseType::create(['name'=>'Office']);
          ExpenseType::create(['name'=>'Salary']);
          ExpenseType::create(['name'=>'Others']);
+         ExpenseType::create(['name'=>'Carry fees']);
     }
 }
