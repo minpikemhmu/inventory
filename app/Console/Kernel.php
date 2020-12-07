c033c3735e4f30dba1a746d7893bc59d4469fb74
minpikehmu <minpikehmu10@gmail.com>
2020-12-07 18:45:21 +0630

    cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('way:delete')->everyMinute();
+        $schedule->command('way:delete')->daily();
     }
 
     /**
